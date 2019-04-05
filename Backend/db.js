7508f43af9fa612870302fddaea5ec76460ddeb0
Alexeev-Andrew <andrey.alexeev.1998@gmail.com>
2019-04-06 00:39:55 +0300

    db methods

--- Backend/db.js
+++ Backend/db.js
@@ -7,8 +7,8 @@ exports.connect = function() {
 
     connection = mysql.createConnection({
         host: 'localhost',
-        user: 'root',
-        password: 'Vaao095037448182',
+        user: 'tracktop',
+        password: 'tracktop123'
     });
 
     connection.connect(function(err) {
@@ -30,3 +30,28 @@ exports.insert_type_of_technics = function(type_of_technics,callback){
     connection.query("INSERT INTO tracktop.types_of_technics SET ?", type_of_technics, callback);
 }
 
+exports.insert_marks_of_technics = function(mark_of_technics,callback){
+    connection.query("INSERT INTO tracktop.marks_of_technics SET ?", mark_of_technics, callback);
+}
+
+exports.insert_client = function(client,callback){
+    connection.query("INSERT INTO tracktop.clients SET ?", client, callback);
+}
+
+exports.insert_provider = function(provider,callback){
+    connection.query("INSERT INTO tracktop.providers SET ?", provider, callback);
+}
+
+exports.insert_check = function(check,callback){
+    connection.query("INSERT INTO tracktop.checks SET ?", check, callback);
+}
+
+//select methods
+
+exports.get_types_of_technics = function(callback){
+    connection.query("SELECT * FROM tracktop.types_of_technics",callback);
+}
+
+exports.get_marks_of_technics = function(callback){
+    connection.query("SELECT * FROM tracktop.marks_of_technics",callback);
+}
