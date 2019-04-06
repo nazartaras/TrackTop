a67dd11640bd1c89f14f64c16b22dc5407e1b665
Alexeev-Andrew <andrey.alexeev.1998@gmail.com>
2019-04-06 15:53:16 +0300

    frontend init commit

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,19 +1,38 @@
 $(function(){
     //This code will execute when the page is ready
-    $('.testSql').click(function(){
+    // $('.testSql').click(function(){
+    //
+    //     var newT = {
+    //         mark_id:1,
+    //         type_id:1,
+    //         amount:32,
+    //         price:221,
+    //         // model:'AD129'
+    //     }
+    //
+    //     require("./API").addTehnic(newT,function (err,data) {
+    //         if(data.error) console.log(data.error);
+    //     });
+    // });
 
-        var newT = {
-            mark_id:1,
-            type_id:1,
-            amount:32,
-            price:221,
-            // model:'AD129'
-        }
+    function openNav() {
+        $("#basketColumn").addClass("widthR");
+        $("#main").addClass("margR");
+    }
 
-        require("./API").addTehnic(newT,function (err,data) {
-            if(data.error) console.log(data.error);
-        });
-    });
+    function closeNav() {
+        $("#basketColumn").removeClass("widthR");
+        $("#main").removeClass("margR");
+    }
+
+    $('.basketBtn').click(function () {
+        openNav();
+    })
+
+    $('.basketCloseBtn').click(function () {
+        closeNav();
+    })
+
 
 
 });
--- Frontend/www/assets/js/main.js
+++ Frontend/www/assets/js/main.js
@@ -1,56 +1,40 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var API_URL = "http://localhost:5050";
-
-function backendGet(url, callback) {
-    $.ajax({
-        url: API_URL + url,
-        type: 'GET',
-        success: function(data){
-            callback(null, data);
-        },
-        error: function() {
-            callback(new Error("Ajax Failed"));
-        }
-    })
-}
-
-function backendPost(url, data, callback) {
-    $.ajax({
-        url: API_URL + url,
-        type: 'POST',
-        contentType : 'application/json',
-        data: JSON.stringify(data),
-        success: function(data){
-            callback(null, data);
-        },
-        error: function() {
-            callback(new Error("Ajax Failed"));
-        }
-    })
-}
-
-exports.addTehnic = function(tehnic, callback) {
-    backendPost("/api/addtechnic/", tehnic, callback);
-};
-
-},{}],2:[function(require,module,exports){
 $(function(){
     //This code will execute when the page is ready
-    $('.testSql').click(function(){
+    // $('.testSql').click(function(){
+    //
+    //     var newT = {
+    //         mark_id:1,
+    //         type_id:1,
+    //         amount:32,
+    //         price:221,
+    //         // model:'AD129'
+    //     }
+    //
+    //     require("./API").addTehnic(newT,function (err,data) {
+    //         if(data.error) console.log(data.error);
+    //     });
+    // });
 
-        var newT = {
-            mark_id:1,
-            type_id:1,
-            amount:32,
-            price:221,
-            // model:'AD129'
-        }
+    function openNav() {
+        $("#basketColumn").addClass("widthR");
+        $("#main").addClass("margR");
+    }
 
-        require("./API").addTehnic(newT,function (err,data) {
-            if(data.error) console.log(data.error);
-        });
-    });
+    function closeNav() {
+        $("#basketColumn").removeClass("widthR");
+        $("#main").removeClass("margR");
+    }
+
+    $('.basketBtn').click(function () {
+        openNav();
+    })
+
+    $('.basketCloseBtn').click(function () {
+        closeNav();
+    })
+
 
 
 });
-},{"./API":1}]},{},[2]);
+},{}]},{},[1]);
