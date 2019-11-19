d1bb5b4a322810b12bf7a3d878c6f88d328e9291
Alexeev-Andrew <andrey.alexeev.1998@gmail.com>
2019-11-19 20:39:16 +0200

    update CORS policy

--- Backend/main.js
+++ Backend/main.js
@@ -17,7 +17,7 @@ function configureEndpoints(app) {
     app.all('*', function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
         next();
     });
 
